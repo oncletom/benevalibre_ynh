64cc6078622b5575e60f80a02e4bdfa74078fff1
Thomas Parisot <oncletom@users.noreply.github.com>
2020-12-30 12:20:30 +0100

    Add install script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="python3-venv python3-dev python3-pip python3-mysqldb libmariadb-dev"
 
 #=================================================
 # PERSONAL HELPERS
